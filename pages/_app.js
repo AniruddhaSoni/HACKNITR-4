02d34b3cad081b1a91a809180b4733dad4ebd5c3
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-07 16:55:08 +0530

    init nEXT project  & All libraries and basic files needed for project

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import "../styles/tailwind_config.css";
+import "../styles/globals.css";
+import "../styles/custom.css";
+import { SessionProvider } from "next-auth/react";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider
+      session={session}
+      refetchOnWindowFocus={false}
+      refetchInterval={10 * 60}
+    >
+      <main className="custom-font min-h-screen w-full bg-slate-50 text-slate-900">
+        <Component {...pageProps} />
+      </main>
+    </SessionProvider>
+  );
+}
